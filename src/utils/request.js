165bfdff3db8882b6a6defcf15b96981fd6934e2
lxcxl <lxcxl647000@gmail.com>
2024-12-06 16:23:21 +0800

    1.封装request请求模块 2.新增环境变量配置

---
+++ src/utils/request.js
@@ -0,0 +1,27 @@
+const { default: axios } = require("axios");
+
+let request = axios.create({
+    baseURL: import.meta.env.VITE_SERVER,
+    timeout: 5000
+});
+
+// 添加请求拦截器
+http.interceptors.request.use((config) => {
+    return config
+}, (error) => {
+    return Promise.reject(error)
+})
+
+// 添加响应拦截器
+http.interceptors.response.use((response) => {
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    return response.data
+}, (error) => {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    return Promise.reject(error)
+})
+
+
+export default request;
