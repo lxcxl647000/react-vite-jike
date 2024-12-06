748be4fec1fd768d824954dca2e718ae9ce34f74
lxcxl <lxcxl647000@gmail.com>
2024-12-06 17:57:45 +0800

    定义token字段常量

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,12 +1,14 @@
 // 本地持久化token//
+const TOKEN = 'TOKEN';
+
 export const SET_TOKEN = (token) => {
-    localStorage.setItem('TOKEN', token);
+    localStorage.setItem(TOKEN, token);
 };
 
 export const GET_TOKEN = () => {
-    return localStorage.getItem('TOKEN');
+    return localStorage.getItem(TOKEN);
 };
 
 export const REMOVE_TOKEN = () => {
-    localStorage.removeItem('TOKEN');
+    localStorage.removeItem(TOKEN);
 };
